5b47116a8203c481693fab02ec3cf8c0b52fcc4e
isigov <isigov@bu.edu>
2015-12-11 18:35:15 -0500

    gpa display

--- My_Grades/app/src/main/java/ec327_final_project/my_grades/gpa_information_display.java
+++ My_Grades/app/src/main/java/ec327_final_project/my_grades/gpa_information_display.java
@@ -1,5 +1,6 @@
 package ec327_final_project.my_grades;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,6 +10,8 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class gpa_information_display extends AppCompatActivity {
 
@@ -17,22 +20,450 @@ public class gpa_information_display extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gpa_information_display);
 
-        String[] gpa_display_options = {"Overall GPA", "Major GPA", "Estimated Overall GPA",
-                "Estimated Major GPA"};
+        SharedPreferences settings = getApplicationContext().getSharedPreferences("My_Grades", 0);
+        int course_count = settings.getInt("Course_Count", -1);
 
-        final ListAdapter gpa_display_options_adapter = new ArrayAdapter<String>(this,R.layout.display_gpa_functions_layout,R.id.display_gpa_totals_text_view);
+        String grid = "";
+        int grid_weight = 0;
+        String grid_major = "";
+
+        String grid2 = "";
+        int grid_weight2 = 0;
+        String grid_major2 = "";
+
+        String grid3 = "";
+        int grid_weight3 = 0;
+        String grid_major3 = "";
+
+        String grid4 = "";
+        int grid_weight4 = 0;
+        String grid_major4 = "";
+
+        String grid5 = "";
+        int grid_weight5 = 0;
+        String grid_major5 = "";
+
+        double course = 0, course2 = 0, course3 = 0, course4 = 0, course5 = 0;
+        if(course_count >= 1)
+        {
+            grid = settings.getString("grid_0_name", "n/a");
+            grid_weight = Integer.parseInt(settings.getString("grid_0_weight", "n/a"));
+            grid_major = settings.getString("grid_0_major", "n/a");
+
+            SharedPreferences assignment = getApplicationContext().getSharedPreferences("My_Grades_" + grid, 0);
+            int assignment_count = assignment.getInt("Assignment_Count", -1);
+
+            String assgrid = "";
+            int assgrid_weight = 0;
+            int assgrid_percent = 0;
+
+            String assgrid2 = "";
+            int assgrid_weight2 = 0;
+            int assgrid_percent2 = 0;
+
+            String assgrid3 = "";
+            int assgrid_weight3 = 0;
+            int assgrid_percent3 = 0;
+
+            String assgrid4 = "";
+            int assgrid_weight4 = 0;
+            int assgrid_percent4 = 0;
+
+            String assgrid5 = "";
+            int assgrid_weight5 = 0;
+            int assgrid_percent5 = 0;
+
+            if(assignment_count >= 1)
+            {
+                assgrid = assignment.getString("grid_0_name", "n/a");
+                assgrid_weight = Integer.parseInt(settings.getString("grid_0_weight", "n/a"));
+                assgrid_percent = Integer.parseInt(assignment.getString("grid_0_percent", "n/a"));
+            }
+            if(assignment_count >= 2)
+            {
+                assgrid2 = assignment.getString("grid_2_name", "n/a");
+                assgrid_weight2 = Integer.parseInt(settings.getString("grid_2_weight", "n/a"));
+                assgrid_percent2 = Integer.parseInt(assignment.getString("grid_2_percent", "n/a"));
+            }
+            if(assignment_count >= 3)
+            {
+                assgrid3 = assignment.getString("grid_3_name", "n/a");
+                assgrid_weight3 = Integer.parseInt(settings.getString("grid_3_weight", "n/a"));
+                assgrid_percent3 = Integer.parseInt(assignment.getString("grid_3_percent", "n/a"));
+            }
+            if(assignment_count >= 4)
+            {
+                assgrid4 = assignment.getString("grid_4_name", "n/a");
+                assgrid_weight4 = Integer.parseInt(settings.getString("grid_4_weight", "n/a"));
+                assgrid_percent4 = Integer.parseInt(assignment.getString("grid_4_percent", "n/a"));
+            }
+            if(assignment_count >= 5)
+            {
+                assgrid5 = assignment.getString("grid_5_name", "n/a");
+                assgrid_weight5 = Integer.parseInt(settings.getString("grid_5_weight", "n/a"));
+                assgrid_percent5 = Integer.parseInt(assignment.getString("grid_5_percent", "n/a"));
+            }
+
+            if(assgrid_weight > 0)
+            {
+                course = course + (assgrid_percent/100) * (assgrid_weight);
+            }
+            if(assgrid_weight2 > 0)
+            {
+                course = course + (assgrid_percent2/100) * (assgrid_weight2);
+            }
+            if(assgrid_weight3 > 0)
+            {
+                course = course + (assgrid_percent3/100) * (assgrid_weight3);
+            }
+            if(assgrid_weight4 > 0)
+            {
+                course = course + (assgrid_percent4/100) * (assgrid_weight4);
+            }
+            if(assgrid_weight5 > 0)
+            {
+                course = course + (assgrid_percent5/100) * (assgrid_weight5);
+            }
+
+        }
+        if(course_count >= 2)
+        {
+            grid2 = settings.getString("grid_2_name", "n/a");
+            grid_weight2 = Integer.parseInt(settings.getString("grid_2_weight", "n/a"));
+            grid_major2 = settings.getString("grid_2_major", "n/a");
+
+            SharedPreferences assignment = getApplicationContext().getSharedPreferences("My_Grades_" + grid, 0);
+            int assignment_count = assignment.getInt("Assignment_Count", -1);
+
+            String assgrid = "";
+            int assgrid_weight = 0;
+            int assgrid_percent = 0;
+
+            String assgrid2 = "";
+            int assgrid_weight2 = 0;
+            int assgrid_percent2 = 0;
+
+            String assgrid3 = "";
+            int assgrid_weight3 = 0;
+            int assgrid_percent3 = 0;
+
+            String assgrid4 = "";
+            int assgrid_weight4 = 0;
+            int assgrid_percent4 = 0;
+
+            String assgrid5 = "";
+            int assgrid_weight5 = 0;
+            int assgrid_percent5 = 0;
+
+            if(assignment_count >= 1)
+            {
+                assgrid = assignment.getString("grid_0_name", "n/a");
+                assgrid_weight = Integer.parseInt(settings.getString("grid_0_weight", "n/a"));
+                assgrid_percent = Integer.parseInt(assignment.getString("grid_0_percent", "n/a"));
+            }
+            if(assignment_count >= 2)
+            {
+                assgrid2 = assignment.getString("grid_2_name", "n/a");
+                assgrid_weight2 = Integer.parseInt(settings.getString("grid_2_weight", "n/a"));
+                assgrid_percent2 = Integer.parseInt(assignment.getString("grid_2_percent", "n/a"));
+            }
+            if(assignment_count >= 3)
+            {
+                assgrid3 = assignment.getString("grid_3_name", "n/a");
+                assgrid_weight3 = Integer.parseInt(settings.getString("grid_3_weight", "n/a"));
+                assgrid_percent3 = Integer.parseInt(assignment.getString("grid_3_percent", "n/a"));
+            }
+            if(assignment_count >= 4)
+            {
+                assgrid4 = assignment.getString("grid_4_name", "n/a");
+                assgrid_weight4 = Integer.parseInt(settings.getString("grid_4_weight", "n/a"));
+                assgrid_percent4 = Integer.parseInt(assignment.getString("grid_4_percent", "n/a"));
+            }
+            if(assignment_count >= 5)
+            {
+                assgrid5 = assignment.getString("grid_5_name", "n/a");
+                assgrid_weight5 = Integer.parseInt(settings.getString("grid_5_weight", "n/a"));
+                assgrid_percent5 = Integer.parseInt(assignment.getString("grid_5_percent", "n/a"));
+            }
+
+            if(assgrid_weight > 0)
+            {
+                course2 = course2 + (assgrid_percent/100) * (assgrid_weight);
+            }
+            if(assgrid_weight2 > 0)
+            {
+                course2 = course2 + (assgrid_percent2/100) * (assgrid_weight2);
+            }
+            if(assgrid_weight3 > 0)
+            {
+                course2 = course2 + (assgrid_percent3/100) * (assgrid_weight3);
+            }
+            if(assgrid_weight4 > 0)
+            {
+                course2 = course2 + (assgrid_percent4/100) * (assgrid_weight4);
+            }
+            if(assgrid_weight5 > 0)
+            {
+                course2 = course2 + (assgrid_percent5/100) * (assgrid_weight5);
+            }
+
+        }
+        if(course_count >= 3)
+        {
+            grid3 = settings.getString("grid_3_name", "n/a");
+            grid_weight3 = Integer.parseInt(settings.getString("grid_3_weight", "n/a"));
+            grid_major3 = settings.getString("grid_3_major", "n/a");
+
+            SharedPreferences assignment = getApplicationContext().getSharedPreferences("My_Grades_" + grid, 0);
+            int assignment_count = assignment.getInt("Assignment_Count", -1);
+
+            String assgrid = "";
+            int assgrid_weight = 0;
+            int assgrid_percent = 0;
+
+            String assgrid2 = "";
+            int assgrid_weight2 = 0;
+            int assgrid_percent2 = 0;
+
+            String assgrid3 = "";
+            int assgrid_weight3 = 0;
+            int assgrid_percent3 = 0;
+
+            String assgrid4 = "";
+            int assgrid_weight4 = 0;
+            int assgrid_percent4 = 0;
+
+            String assgrid5 = "";
+            int assgrid_weight5 = 0;
+            int assgrid_percent5 = 0;
+
+            if(assignment_count >= 1)
+            {
+                assgrid = assignment.getString("grid_0_name", "n/a");
+                assgrid_weight = Integer.parseInt(settings.getString("grid_0_weight", "n/a"));
+                assgrid_percent = Integer.parseInt(assignment.getString("grid_0_percent", "n/a"));
+            }
+            if(assignment_count >= 2)
+            {
+                assgrid2 = assignment.getString("grid_2_name", "n/a");
+                assgrid_weight2 = Integer.parseInt(settings.getString("grid_2_weight", "n/a"));
+                assgrid_percent2 = Integer.parseInt(assignment.getString("grid_2_percent", "n/a"));
+            }
+            if(assignment_count >= 3)
+            {
+                assgrid3 = assignment.getString("grid_3_name", "n/a");
+                assgrid_weight3 = Integer.parseInt(settings.getString("grid_3_weight", "n/a"));
+                assgrid_percent3 = Integer.parseInt(assignment.getString("grid_3_percent", "n/a"));
+            }
+            if(assignment_count >= 4)
+            {
+                assgrid4 = assignment.getString("grid_4_name", "n/a");
+                assgrid_weight4 = Integer.parseInt(settings.getString("grid_4_weight", "n/a"));
+                assgrid_percent4 = Integer.parseInt(assignment.getString("grid_4_percent", "n/a"));
+            }
+            if(assignment_count >= 5)
+            {
+                assgrid5 = assignment.getString("grid_5_name", "n/a");
+                assgrid_weight5 = Integer.parseInt(settings.getString("grid_5_weight", "n/a"));
+                assgrid_percent5 = Integer.parseInt(assignment.getString("grid_5_percent", "n/a"));
+            }
+
+            if(assgrid_weight > 0)
+            {
+                course3 = course3 + (assgrid_percent/100) * (assgrid_weight);
+            }
+            if(assgrid_weight2 > 0)
+            {
+                course3 = course3 + (assgrid_percent2/100) * (assgrid_weight2);
+            }
+            if(assgrid_weight3 > 0)
+            {
+                course3 = course3 + (assgrid_percent3/100) * (assgrid_weight3);
+            }
+            if(assgrid_weight4 > 0)
+            {
+                course3 = course3 + (assgrid_percent4/100) * (assgrid_weight4);
+            }
+            if(assgrid_weight5 > 0)
+            {
+                course3 = course3 + (assgrid_percent5/100) * (assgrid_weight5);
+            }
+
+        }
+        if(course_count >= 4)
+        {
+            grid4 = settings.getString("grid_4_name", "n/a");
+            grid_weight4 = Integer.parseInt(settings.getString("grid_4_weight", "n/a"));
+            grid_major4 = settings.getString("grid_4_major", "n/a");
+
+            SharedPreferences assignment = getApplicationContext().getSharedPreferences("My_Grades_" + grid, 0);
+            int assignment_count = assignment.getInt("Assignment_Count", -1);
+
+            String assgrid = "";
+            int assgrid_weight = 0;
+            int assgrid_percent = 0;
+
+            String assgrid2 = "";
+            int assgrid_weight2 = 0;
+            int assgrid_percent2 = 0;
+
+            String assgrid3 = "";
+            int assgrid_weight3 = 0;
+            int assgrid_percent3 = 0;
+
+            String assgrid4 = "";
+            int assgrid_weight4 = 0;
+            int assgrid_percent4 = 0;
+
+            String assgrid5 = "";
+            int assgrid_weight5 = 0;
+            int assgrid_percent5 = 0;
+
+            if(assignment_count >= 1)
+            {
+                assgrid = assignment.getString("grid_0_name", "n/a");
+                assgrid_weight = Integer.parseInt(settings.getString("grid_0_weight", "n/a"));
+                assgrid_percent = Integer.parseInt(assignment.getString("grid_0_percent", "n/a"));
+            }
+            if(assignment_count >= 2)
+            {
+                assgrid2 = assignment.getString("grid_2_name", "n/a");
+                assgrid_weight2 = Integer.parseInt(settings.getString("grid_2_weight", "n/a"));
+                assgrid_percent2 = Integer.parseInt(assignment.getString("grid_2_percent", "n/a"));
+            }
+            if(assignment_count >= 3)
+            {
+                assgrid3 = assignment.getString("grid_3_name", "n/a");
+                assgrid_weight3 = Integer.parseInt(settings.getString("grid_3_weight", "n/a"));
+                assgrid_percent3 = Integer.parseInt(assignment.getString("grid_3_percent", "n/a"));
+            }
+            if(assignment_count >= 4)
+            {
+                assgrid4 = assignment.getString("grid_4_name", "n/a");
+                assgrid_weight4 = Integer.parseInt(settings.getString("grid_4_weight", "n/a"));
+                assgrid_percent4 = Integer.parseInt(assignment.getString("grid_4_percent", "n/a"));
+            }
+            if(assignment_count >= 5)
+            {
+                assgrid5 = assignment.getString("grid_5_name", "n/a");
+                assgrid_weight5 = Integer.parseInt(settings.getString("grid_5_weight", "n/a"));
+                assgrid_percent5 = Integer.parseInt(assignment.getString("grid_5_percent", "n/a"));
+            }
+
+            if(assgrid_weight > 0)
+            {
+                course4 = course4 + (assgrid_percent/100) * (assgrid_weight);
+            }
+            if(assgrid_weight2 > 0)
+            {
+                course4 = course4 + (assgrid_percent2/100) * (assgrid_weight2);
+            }
+            if(assgrid_weight3 > 0)
+            {
+                course4 = course4 + (assgrid_percent3/100) * (assgrid_weight3);
+            }
+            if(assgrid_weight4 > 0)
+            {
+                course4 = course4 + (assgrid_percent4/100) * (assgrid_weight4);
+            }
+            if(assgrid_weight5 > 0)
+            {
+                course4 = course4 + (assgrid_percent5/100) * (assgrid_weight5);
+            }
+
+        }
+        if(course_count >= 5)
+        {
+            grid5 = settings.getString("grid_5_name", "n/a");
+            grid_weight5 = Integer.parseInt(settings.getString("grid_5_weight", "n/a"));
+            grid_major5 = settings.getString("grid_5_major", "n/a");
+
+            SharedPreferences assignment = getApplicationContext().getSharedPreferences("My_Grades_" + grid, 0);
+            int assignment_count = assignment.getInt("Assignment_Count", -1);
+
+            String assgrid = "";
+            int assgrid_weight = 0;
+            int assgrid_percent = 0;
+
+            String assgrid2 = "";
+            int assgrid_weight2 = 0;
+            int assgrid_percent2 = 0;
+
+            String assgrid3 = "";
+            int assgrid_weight3 = 0;
+            int assgrid_percent3 = 0;
+
+            String assgrid4 = "";
+            int assgrid_weight4 = 0;
+            int assgrid_percent4 = 0;
+
+            String assgrid5 = "";
+            int assgrid_weight5 = 0;
+            int assgrid_percent5 = 0;
 
 
-        ListView gpa_display_options_ListView = (ListView) findViewById(R.id.gpa_information_list_view);
+            if(assignment_count >= 1)
+            {
+                assgrid = assignment.getString("grid_0_name", "n/a");
+                assgrid_weight = Integer.parseInt(settings.getString("grid_0_weight", "n/a"));
+                assgrid_percent = Integer.parseInt(assignment.getString("grid_0_percent", "n/a"));
+            }
+            if(assignment_count >= 2)
+            {
+                assgrid2 = assignment.getString("grid_2_name", "n/a");
+                assgrid_weight2 = Integer.parseInt(settings.getString("grid_2_weight", "n/a"));
+                assgrid_percent2 = Integer.parseInt(assignment.getString("grid_2_percent", "n/a"));
+            }
+            if(assignment_count >= 3)
+            {
+                assgrid3 = assignment.getString("grid_3_name", "n/a");
+                assgrid_weight3 = Integer.parseInt(settings.getString("grid_3_weight", "n/a"));
+                assgrid_percent3 = Integer.parseInt(assignment.getString("grid_3_percent", "n/a"));
+            }
+            if(assignment_count >= 4)
+            {
+                assgrid4 = assignment.getString("grid_4_name", "n/a");
+                assgrid_weight4 = Integer.parseInt(settings.getString("grid_4_weight", "n/a"));
+                assgrid_percent4 = Integer.parseInt(assignment.getString("grid_4_percent", "n/a"));
+            }
+            if(assignment_count >= 5)
+            {
+                assgrid5 = assignment.getString("grid_5_name", "n/a");
+                assgrid_weight5 = Integer.parseInt(settings.getString("grid_5_weight", "n/a"));
+                assgrid_percent5 = Integer.parseInt(assignment.getString("grid_5_percent", "n/a"));
+            }
 
-        gpa_display_options_ListView.setAdapter(gpa_display_options_adapter);
+            if(assgrid_weight > 0)
+            {
+                course5 = course5 + (assgrid_percent/100) * (assgrid_weight);
+            }
+            if(assgrid_weight2 > 0)
+            {
+                course5 = course5 + (assgrid_percent2/100) * (assgrid_weight2);
+            }
+            if(assgrid_weight3 > 0)
+            {
+                course5 = course5 + (assgrid_percent3/100) * (assgrid_weight3);
+            }
+            if(assgrid_weight4 > 0)
+            {
+                course5 = course5 + (assgrid_percent4/100) * (assgrid_weight4);
+            }
+            if(assgrid_weight5 > 0)
+            {
+                course5 = course5 + (assgrid_percent5/100) * (assgrid_weight5);
+            }
 
+        }
 
+        course = (course * 4.0)/100;
+        course2 = (course2 * 4.0)/100;
+        course3 = (course3 * 4.0)/100;
+        course4 = (course4 * 4.0)/100;
+        course5 = (course5 * 4.0)/100;
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        
+        //double gpa = course
+        Toast.makeText(getApplicationContext(), "Calculated data", Toast.LENGTH_SHORT).show();
     }
 
 }
